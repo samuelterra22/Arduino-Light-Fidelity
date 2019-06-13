73bf7096d11e424f3cd753913dc7b3f3cdb6bf88
Samuel Terra <samuelterra22@hotmail.com>
2019-06-13 14:45:34 -0300

    Add code for transmitter

--- Transmitter/transmitter.ino
+++ Transmitter/transmitter.ino
@@ -1,11 +1,16 @@
-// Transmitter code
+#include <SoftwareSerial.h>
 
+SoftwareSerial SerialCriada(2, 3); // RX, TX.
 void setup() {
-  // put your setup code here, to run once:
-
+    // Abre a serial para enviar dados
+    Serial.begin(9600);// Velocidade de 9600 bits por segundo.
+    Serial.println("Envie o que quiser!");
+    SerialCriada.begin(4800);// Velocidade de 4800 bits por segundo.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    if (Serial.available()) {// Se a serial tiver dados em seu buffer.
+        SerialCriada.write(Serial.read());//Escreve na serial dados a SerialCriada
+    }
 }
+
