e1e5aafc9b30e9edbf101f0189b8757155cce0b0
Samuel Terra <samuelterra22@hotmail.com>
2019-06-13 14:45:05 -0300

    Add code for receptor

--- Receptor/receptor.ino
+++ Receptor/receptor.ino
@@ -1,12 +1,15 @@
-// Receptor code
+#include <SoftwareSerial.h>
 
+SoftwareSerial SerialCriada(2, 3); // RX, TX
 void setup() {
-  // put your setup code here, to run once:
-
+    // Abre a serial para receber dados
+    Serial.begin(9600);// Velocidade de 960000 bits por segundo.
+    Serial.println("Aguarde pelos dados.");
+    SerialCriada.begin(4800);// Velocidade de 4800 bits por segundo.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    if (SerialCriada.available()) {// Se a serial tiver dados em seu buffer.
+        Serial.write(SerialCriada.read());//Escreve na serial dados a SerialCriada
+    }
 }
-
